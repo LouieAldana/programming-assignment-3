5a07d5c06d1a5bf360a491ef681b7dd5a851e327
Louie Aldana <louiealdana@hotmail.com>
2021-02-26 23:20:52 -0800

    fixed css code

--- src/component/page2.js
+++ src/component/page2.js
@@ -1,8 +1,29 @@
+import { Component } from 'react';
 import React from "react"
 import './style.css';
 
 
+const lightbox = document.createElement('div')
+lightbox.id = 'lightbox'
+document.body.appendChild(lightbox)
 
+const images = document.querySelectorAll('img')
+images.forEach(image => {
+    image.addEventListener('click', e => {
+        lightbox.classList.add('active')
+        const img = document.createElement('img')
+        img.src = image.src
+        while (lightbox.firstChild) {
+            lightbox.removeChild(lightbox.firstChild)
+        }
+        lightbox.appendChild(img)
+    })
+})
+
+lightbox.addEventListener('click', e => {
+    if (e.target !== e.currentTarget) return
+    lightbox.classList.remove('active')
+})
 
 function Imagedata() {
     var img1 = require('./piglet.img')
@@ -11,6 +32,8 @@ function Imagedata() {
     var img4 = require('./giraffe.img')
     var img5 = require('./drops.img')
     var img6 = require('./lightning.img')
+    var img7 = require('./ladybug.img')
+    var img8 = require('./mantis.img')
     return (
 
         <div>
@@ -24,6 +47,8 @@ function Imagedata() {
                     <img src={img3.default} alt="giraffe" />
                     <img src={img4.default} alt="drops" />
                     <img src={img6.default} alt="lightning" />
+                    <img src={img7.default} alt="ladybug" />
+                    <img src={img8.default} alt="mantis" />
                 </div>
 
             </body>
